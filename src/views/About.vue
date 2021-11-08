80ce7ddbbc476e1d8fc8e2df6be6d2d6ab9fbd38
abdelhamied400 <abdelhamied.mostafa@businessvalley.com>
2021-11-08 15:40:56 +0200

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Button type="primary" size="large">something</Button>
   </div>
 </template>
+
+<script>
+import { Button } from 'ant-design-vue';
+
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style>
+</style>
