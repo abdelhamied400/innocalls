c66eae4783a6fb5a91c92f852ef9f8913b6d060d
abdelhamied400 <abdelhamied.mostafa@businessvalley.com>
2021-11-09 11:21:04 +0200

    style configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#2F8FCF',
+          dark: '#0D4D80',
+        },
+        state: {
+          success: '#52c41a',
+          warning: '#faad14',
+          error: '#f5222d',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const vueConfig = {
   pluginOptions: {
     i18n: {
       locale: 'en',
@@ -10,4 +10,31 @@ module.exports = {
       fullInstall: true,
     },
   },
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          'primary-color': '#2F8FCF',
+          'link-color': '#2F8FCF',
+          'success-color': '#52c41a',
+          'warning-color': '#faad14',
+          'error-color': '#f5222d',
+          'font-size-base': '14px',
+          'heading-color': 'rgba(0, 0, 0, 0.85)',
+          'text-color': 'rgba(0, 0, 0, 0.65)',
+          'text-color-secondary': 'rgba(0, 0, 0, 0.45)',
+          'disabled-color': 'rgba(0, 0, 0, 0.25)',
+          'border-radius-base': '2px',
+          'border-color-base': '#d9d9d9',
+          'box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)',
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
+  devServer: {
+    port: 8000,
+  },
 };
+
+module.exports = vueConfig;
