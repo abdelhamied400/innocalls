82642f9d1bf1014bd368dfe6240c32a4dd066a9f
abdelhamied400 <abdelhamied.mostafa@businessvalley.com>
2021-11-10 14:22:42 +0200

    implementing private routes

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+      <div class="login">
+          <p>login page</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
